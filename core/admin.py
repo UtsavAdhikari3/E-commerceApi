c44da12c05f7818d3d4ceda2983641936f5f2f2e
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2025-07-20 19:52:22 +0545

    abstract user login reg

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (('Custom Fields',{'fields':('age','phone_number')}),)
+
+
+admin.site.register(CustomUser,CustomUserAdmin)
